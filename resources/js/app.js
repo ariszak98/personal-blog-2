e31846493112dd0013b54b4f49ec7dc50c412559
ariszak98 <ariszak98@icloud.com>
2024-06-21 17:27:27 +0300

    Initial commit for Personal Blog 2

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import '../css/base.css';
+import '../css/main.css';
+import '../css/vendor.css';
+
+import './bootstrap';
+import './jquery-3.2.1.min';
+import './modernizr';
+import './plugins';
+import './main';
