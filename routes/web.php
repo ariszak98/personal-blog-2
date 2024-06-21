d6aeb90c9d183212153537dc202f17ffe05e5df0
ariszak98 <ariszak98@icloud.com>
2024-06-21 20:21:16 +0300

    Fixed Design and Top level Routing

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() { return view('index'); });
+Route::get('/', function() { return view('index'); })->name('index');
+Route::get('/about', function() { return view('about'); })->name('about');
+Route::get('/contact', function() { return view('contact'); })->name('contact');
